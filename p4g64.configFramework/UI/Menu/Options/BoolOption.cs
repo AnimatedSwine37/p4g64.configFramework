373ffb90394a363b3d259e2504ba402a6aa355cb
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2024-04-28 17:33:06 +1000

    Implement BoolOption draw

--- p4g64.configFramework/UI/Menu/Options/BoolOption.cs
+++ p4g64.configFramework/UI/Menu/Options/BoolOption.cs
@@ -1,4 +1,5 @@
-﻿using static p4g64.configFramework.UI.Common.Spr;
+﻿using p4g64.configFramework.UI.Common;
+using static p4g64.configFramework.UI.Common.Spr;
 
 namespace p4g64.configFramework.UI.Menu.Options;
 public unsafe class BoolOption : BaseMenuOption
@@ -20,7 +21,25 @@ public unsafe class BoolOption : BaseMenuOption
 
     public override void Draw(float xPos, float yPos, byte alpha, SpriteFile* cMainSpr, bool isSelected)
     {
-        // TODO implement
+        DrawOptionBg(cMainSpr, alpha, xPos, yPos, isSelected);
+        DrawOptionName(alpha, xPos, yPos, isSelected);
+
+
+        if (isSelected)
+        {
+            // Draw the two buttons around the option
+            Spr.DrawWrapped(cMainSpr, 0xa4, xPos + 338.0f - 12.0f, yPos + 7, 0.0f, 0xd7, 0xff, 0xb, alpha, 0.85f, 0.85f, -90.0f);
+            Spr.DrawWrapped(cMainSpr, 0xa4, xPos + 338.0f + 104.0f, yPos + 7, 0.0f, 0xd7, 0xff, 0xb, alpha, 0.85f, 0.85f, 90.0f);
+
+            // Draw the background of the value
+            Spr.Draw(cMainSpr, 0x203, xPos + 338.0f, yPos + 2, 0.0f, 0xd7, 0xff, 0xb, alpha, 1.0f, 0.85f, 0.0f, 0.0f, 0.0f);
+            Spr.Draw(cMainSpr, 0x202, xPos + 338.0f + 104.0f, yPos + 2, 0.0f, 0xd7, 0xff, 0xb, alpha, 1.0f, 0.85f, 0.0f, 0.0f, 0.0f);
+        }
+
+        // Draw ON or OFF
+        var text = Value ? "ON" : "OFF";
+        RevColour textColour = new RevColour { R = 0x2D, G = 0x2D, B = 0x2D, A = alpha };
+        Text.Draw(xPos + 393.0f, yPos + 6, 0, textColour, 0, 1, text, Text.TextPositioning.Center);
     }
 
     public override void KeyPressed(int key)
