52c8434ec822175ec730e62feb7116876395bb66
AnimatedSwine37 <24914353+AnimatedSwine37@users.noreply.github.com>
2024-04-28 19:19:18 +1000

    Implement confirm button draw

--- p4g64.configFramework/UI/Menu/Options/ConfirmButton.cs
+++ p4g64.configFramework/UI/Menu/Options/ConfirmButton.cs
@@ -12,7 +12,16 @@ public class ConfirmButton : BaseMenuOption
         DrawOptionBg(cMainSpr, alpha, xPos, yPos, isSelected);
         DrawOptionName(alpha, xPos, yPos, isSelected);
 
-        // TODO implement
+        if (isSelected)
+        {
+            // Draw the background of the button
+            Spr.Draw(cMainSpr, 0x203, xPos + 338.0f, yPos + 2, 0.0f, 0xd7, 0xff, 0xb, alpha, 1.0f, 0.85f, 0.0f, 0.0f, 0.0f);
+            Spr.Draw(cMainSpr, 0x202, xPos + 338.0f + 104.0f, yPos + 2, 0.0f, 0xd7, 0xff, 0xb, alpha, 1.0f, 0.85f, 0.0f, 0.0f, 0.0f);
+        }
+
+        // Draw OK
+        RevColour textColour = new RevColour { R = 0x2D, G = 0x2D, B = 0x2D, A = alpha };
+        Text.Draw(xPos + 393.0f, yPos + 6, 0, textColour, 0, 1, "OK", Text.TextPositioning.Center);
     }
 
     public override void KeyPressed(int key)
